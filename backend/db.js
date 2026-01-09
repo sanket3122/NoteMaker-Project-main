0e56e97cfa5f1b910112f0b195f4ab069c00ccea
Sanket Jaysing Bendale <sanketbendale25@gmail.com>
2026-01-09 18:49:20 -0500

    logs

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
 // const mongoose = require('mongoose');
 // const mongoURI = "mongodb://localhost:27017/iNotebook";
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv:/iNotebook?retryWrites=true&w=majority&appName=Cluster0";
-
-// mongodb://localhost:27017/
+// const mongoURI = "mongodb+srv:/iNotebook?retryWrites=true&w=majority&appName=Cluster0";
+// const mongoURI = "mongodb+srv://sanketbendale25:fkDs7iPlBvtnXIIj@cluster0.rumcmgj.mongodb.net/iNotebook?retryWrites=true&w=majority&appName=Cluster0";
 
 async function connectToMongo() {
   await mongoose
