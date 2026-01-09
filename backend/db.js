9af90db606d0cf524884eb95769ec522e1fddff6
Sanket Bendale <sanketbendale25@gmail.com>
2026-01-09 16:48:40 -0500

    removed MongoURI

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // const mongoose = require('mongoose');
 // const mongoURI = "mongodb://localhost:27017/iNotebook";
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://sanketbendale25:fkDs7iPlBvtnXIIj@cluster0.rumcmgj.mongodb.net/iNotebook?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = "mongodb+srv:/iNotebook?retryWrites=true&w=majority&appName=Cluster0";
 
 // mongodb://localhost:27017/
 
